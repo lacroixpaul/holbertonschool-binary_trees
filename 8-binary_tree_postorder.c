10739e00d7a1e559cf53c9f4fc0e72781c906dea
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-09 14:51:00 +0100

    binary_tree_height add betty compliant

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,7 +9,6 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-
 	if (tree == NULL || func == NULL)
 		return;
 	binary_tree_postorder(tree->left, func);
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,9 +8,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
-
-return (0);
+	right_height = (binary_tree_height(tree->right) + 1);
+	left_height = (binary_tree_height(tree->left) + 1);
+		if (right_height < left_height)
+			return (left_height);
+		else
+			return (right_height);
 }
