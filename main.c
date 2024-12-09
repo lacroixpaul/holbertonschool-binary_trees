dc2cf12ee962e5ace32bdc4795c89aaf5b5b127b
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-09 15:57:37 +0100

    binary_tree_height else rm

--- main.c
+++ main.c
@@ -17,7 +17,6 @@ int main(void)
     root->right = binary_tree_node(root, 402);
     binary_tree_insert_right(root->left, 54);
     binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
 
     height = binary_tree_height(root);
     printf("Height from %d: %lu\n", root->n, height);
