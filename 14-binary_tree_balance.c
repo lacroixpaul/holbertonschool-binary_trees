0e95cc5ba78e41039c47b53fa872c444117e609f
turtle-nest <n78693107@gmail.com>
2024-12-10 11:46:26 +0100

    Add a different implementation of binary_tree_height taking into account the current node for the return value

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,31 +8,24 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0, max_height;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left != NULL)
-	{
-		left_height = binary_tree_height(tree->left);
-
-	}
-	if (tree->right != NULL)
-	{
-		right_height = binary_tree_height(tree->right);
-	}
-
-	if (left_height > right_height)
-	{
-		max_height = left_height;
-	}
+		left_height = 1 + binary_tree_height(tree->left);
 	else
-	{
-		max_height = right_height;
-	}
+		left_height = 0;
+	if (tree->right != NULL)
+		right_height = 1 + binary_tree_height(tree->right);
+	else
+		right_height = 0;
 
-	return (max_height + 1);
+	if (right_height > left_height)
+		return (right_height + 1);
+	else
+		return (left_height + 1);
 }
 
 /**
