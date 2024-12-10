66f29a1a2f8bbcfd82525103b7837ea8dff8a097
turtle-nest <n78693107@gmail.com>
2024-12-10 14:21:32 +0100

    Correct spaces and tabs to be betty compliant

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,20 +37,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
  **/
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    size_t left_height = 0, right_height = 0;
+	size_t left_height = 0, right_height = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    if (left_height != right_height)
-        return(0);
+	if (left_height != right_height)
+		return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    return (binary_tree_is_perfect(tree->left) && 
-            binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
 }
