3a9682889ae54c8681f15916bb85900668413e19
turtle-nest <n78693107@gmail.com>
2024-12-10 15:25:49 +0100

    Add comment of the function binary_tree_balance to be betty compliant

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,8 +13,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-		left_height = binary_tree_height(tree->left);
-		right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (right_height > left_height)
 		return (right_height + 1);
@@ -23,7 +23,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_nodes - measures the balance factor of a binary tree.
+ * binary_tree_balance - measures the balance factor of a binary tree.
  * @tree: a pointer to the root node of the tree
  * to measure the balance factor.
  *
